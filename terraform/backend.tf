3db09bd2dce409b1d607ce8e2294b6d99d9d2dee
Aaron Reynoza <aaron@reynoza.org>
2025-10-17 23:30:23 -0600

    upgrade proxmox provider version

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,13 @@
 terraform {
-  backend "s3" {}
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox" 
+      version = ">= 2.9.0, < 4.0.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.0"
+    }
+  }
+  required_version = ">= 1.5.0"
 }
