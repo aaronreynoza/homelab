43124a77e3007d215e1e0a9b6a6319d19253edba
Aaron Reynoza <aaron@reynoza.org>
2025-10-17 23:33:40 -0600

    set stable versions for providers

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,13 +1,16 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox" 
-      version = ">= 2.9.0, < 4.0.0"
+      source  = "Telmate/proxmox"
+      version = "~> 2.9"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.0"
+      version = "~> 3.2"
     }
   }
-  required_version = ">= 1.5.0"
+
+  backend "s3" {}
 }
